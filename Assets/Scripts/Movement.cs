a6420f380bc80a9e94cc84e4ad6cdee8e156d99a
Andrew Maher <andrewaacodo12@gmail.com>
2022-02-10 03:05:16 +0200

    Arriving Algorithm

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,18 +4,31 @@ public enum KinematicState { Seek, Flee}
 
 public class Movement : MonoBehaviour
 {
-    private  Rigidbody2D character;
     public Transform target;
+
     public float maxSpeed = 2f;
 
+    public float radius = 1f;
+
+    public float timeToTarget = 1f;
+
     public KinematicState kinematicState = KinematicState.Seek;
+
     private class KinematicOutput
     {
         public Vector3 velocity { get; set; }
         public float rotation { get; set; }
+
+        public void Reset()
+        {
+            velocity = Vector3.zero;
+            rotation = 0f;
+        }
     }
 
     private KinematicOutput kinematicOutput = new KinematicOutput { velocity = Vector3.zero, rotation = 0f };
+
+    private Rigidbody2D character;
 
     private void Awake()
     {
@@ -35,10 +48,23 @@ public class Movement : MonoBehaviour
     {
         // Get Direction of movement
         kinematicOutput.velocity = kinematicState == KinematicState.Seek ?  target.position - character.transform.position : character.transform.position - target.position  ;
-        // Normalize vector 
-        kinematicOutput.velocity = kinematicOutput.velocity.normalized;
-        // Max speed 
-        kinematicOutput.velocity *= maxSpeed;
+
+        if(kinematicOutput.velocity.magnitude < radius)
+        {
+            kinematicOutput.Reset();
+            return;
+        }
+
+        kinematicOutput.velocity /= timeToTarget;
+
+        if(kinematicOutput.velocity.magnitude > maxSpeed)
+        {
+            // Normalize vector 
+            kinematicOutput.velocity = kinematicOutput.velocity.normalized;
+            // Max speed 
+            kinematicOutput.velocity *= maxSpeed;
+        }
+
         // Calculate angle of rotation
         kinematicOutput.rotation = CacluateOrientation(kinematicOutput.velocity);
     }
