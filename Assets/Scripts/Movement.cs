3e89c27f9dc7ad1713d5d22f197e0431b33165da
Andrew Maher <andrewaacodo12@gmail.com>
2022-02-11 15:37:33 +0200

    Wandering  Algorithm

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 
-public enum KinematicState { Seek, Flee}
+public enum KinematicState { Seek, Flee, Wandering}
 
 public class Movement : MonoBehaviour
 {
     public Transform target;
+
+    public float maxTimeToRotate = 5f;
+
+    private float currentTimeToROtate = 0f;
+
+    public float maxRotation = 50f;
 
     public float maxSpeed = 2f;
 
@@ -37,14 +43,36 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        setKinematicOutput();
+        if(kinematicState == KinematicState.Wandering)
+        {
+            currentTimeToROtate += Time.fixedDeltaTime;
+
+            getWanderingOutput();
+        }
+        else
+        {
+            getKinematicOutput();
+        }
 
         character.transform.position += kinematicOutput.velocity * Time.fixedDeltaTime;
 
         character.transform.rotation = Quaternion.AngleAxis(kinematicOutput.rotation, Vector3.forward);
     }
+    
+    private void getWanderingOutput()
+    {
+        // Move in direction of current orientation
+        kinematicOutput.velocity = character.transform.right * maxSpeed;
+        // Modify current orientation
+        if(currentTimeToROtate >= maxTimeToRotate)
+        {
+            kinematicOutput.rotation = Random.Range(-1f, 1f) * maxRotation;
 
-    private void setKinematicOutput()
+            currentTimeToROtate = 0;
+        }
+    }
+
+    private void getKinematicOutput()
     {
         // Get Direction of movement
         kinematicOutput.velocity = kinematicState == KinematicState.Seek ?  target.position - character.transform.position : character.transform.position - target.position  ;
